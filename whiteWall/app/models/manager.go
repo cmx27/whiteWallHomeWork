84cd7820e8d43b384ee6458b522e62881d17fab0
cmx27 <zyx20041227@outlook.com>
2023-10-03 08:50:59 +0800

    deleted:    whiteWall/app/controllers/managerControllers/delect.go 	deleted:    whiteWall/app/controllers/managerControllers/get.go 	deleted:    whiteWall/app/controllers/studentControllers/articalControllers/create.go 	deleted:    whiteWall/app/controllers/studentControllers/articalControllers/delete.go 	deleted:    whiteWall/app/controllers/studentControllers/articalControllers/get.go 	deleted:    whiteWall/app/controllers/studentControllers/articalControllers/update.go 	deleted:    whiteWall/app/controllers/studentControllers/imageControllers/image.go 	deleted:    whiteWall/app/controllers/userControllers/login.go 	deleted:    whiteWall/app/controllers/userControllers/register.go 	deleted:    whiteWall/app/midwares/handleNotFound.go 	deleted:    whiteWall/app/models/artical.go 	deleted:    whiteWall/app/models/manager.go 	deleted:    whiteWall/app/models/user.go 	deleted:    whiteWall/app/services/managerServices/delect.go 	deleted:    whiteWall/app/services/managerServices/qurey.go 	deleted:    whiteWall/app/services/studentServices/articalServices/check.go 	deleted:    whiteWall/app/services/studentServices/articalServices/create.go 	deleted:    whiteWall/app/services/studentServices/articalServices/delete.go 	deleted:    whiteWall/app/services/studentServices/articalServices/query.go 	deleted:    whiteWall/app/services/studentServices/articalServices/update.go 	deleted:    whiteWall/app/services/userServices/check.go 	deleted:    whiteWall/app/services/userServices/configServices/config.go 	deleted:    whiteWall/app/services/userServices/create.go 	deleted:    whiteWall/app/services/userServices/query.go 	deleted:    whiteWall/app/services/userServices/session.go 	deleted:    whiteWall/app/utils/encrypt.go 	deleted:    whiteWall/app/utils/jsonResponse.go 	deleted:    whiteWall/config.yaml 	deleted:    whiteWall/config/config/config.go 	deleted:    whiteWall/config/corsConfig/corsConfig.go 	deleted:    whiteWall/config/database/autoMigrate.go 	deleted:    whiteWall/config/database/database.go 	deleted:    whiteWall/config/redis/config.go 	deleted:    whiteWall/config/redis/redis.go 	deleted:    whiteWall/config/router/router.go 	deleted:    whiteWall/go.mod 	deleted:    whiteWall/go.sum 	deleted:    whiteWall/main.go 	deleted:    "whiteWall/\345\244\207\345\277\230\345\275\225.md"

--- whiteWall/app/models/manager.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Manager struct {
-	ManagerID    uint   `json:"manager_id" gorm:"primary_key;AUTO_INCREMENT"`
-	Account      string `json:"account"`
-	Password     string `json:"-"`
-	Name         string `json:"name"`
-	ManagerState bool   `json:"manager_state"`
-}
